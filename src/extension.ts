2d3a8b4807e81e03d8d8642551eaaefe4826292d
Patrick Webster <Patrick.Webster@emc.com>
2016-03-02 13:22:19 -0400

    Fixed issue with node_modules

--- src/extension.ts
+++ src/extension.ts
@@ -10,22 +10,22 @@ import {listExtensions} from './galleryService';
 export function activate(context: vscode.ExtensionContext) {
     const extensionService = new ExtensionService();
 
-    let installMissingExtensions = vscode.commands.registerCommand('extension.installMissing', () => {
-        extensionService.listMissingExtensions().then(extensions => {;
+    let installMissingExtensions = vscode.commands.registerCommand('webstp.extension-manager.installMissing', () => {
+        extensionService.listMissingExtensions().then(extensions => {
             if (extensions.length === 0) {
                 vscode.window.showInformationMessage("All extensions installed");
             }
             else {
                 listExtensions(extensions).then(extensions => {
                     const total = extensions.length;
-                    const promises: Promise<{}>[] = [];
-                    let finished = 0;
+                    const promises: Promise<any>[] = [];
+                    
                     extensions.forEach(e => {
                         promises.push(extensionService.installExtension(e));
                     });
 
                     Promise.all(promises).then(() => {
-                        vscode.window.showInformationMessage(`Finished installing ${finished} extensions`);
+                        vscode.window.showInformationMessage(`Finished installing ${total} extensions`);
                     });
                 });
             }
